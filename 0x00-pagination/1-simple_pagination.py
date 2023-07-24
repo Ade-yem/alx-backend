00eb4d77e6c57b510cfc02b55f054a8781c4ca0c
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-07-24 23:10:41 +0100

    update get_page function

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -37,8 +37,8 @@ class Server:
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
+        dataset = self.dataset()
         try:
-            dataset = self.dataset()
             rng = index_range(page, page_size)
             return dataset[rng[0]:rng[1]]
         except IndexError:
