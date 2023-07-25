3cfa9d72bb583003956edabeb20cda1ece3bd0dc
Adeyemi Adejumo <93265896+Ade-yem@users.noreply.github.com>
2023-07-25 19:52:47 +0100

    Update 2-hypermedia_pagination.py

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -35,7 +35,11 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Get the contents of a page"""
+        """Get the contents of a page
+        Args:
+            page: page number
+            page_size: size of the page
+        """
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
         try:
