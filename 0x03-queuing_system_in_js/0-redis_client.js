4884dc3eb53976ea77d64d7965977d281e8357a5
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-08-29 21:38:09 +0100

    moving to local machine

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,5 +1,14 @@
+// 0-redis_op.js
+
 import { createClient } from "redis";
 
-const client = createClient();
-client.on('error', err => console.log('Redis client not connected to the server: ', err));
-client.on('connect', () => console.log('Redis client connected to the server'));
+// Create a Redis client instance connecting to localhost
+const client = createClient({});
+
+client.on("connect", () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on("error", (error) => {
+  console.error('Redis client not connected to the server:', error);
+});
